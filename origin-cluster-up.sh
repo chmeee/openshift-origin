80aeb5bc89e8242bb232b8a812fa01798ea80fd6
José Manuel Pavón Álvarez <jmpavon@intellimentsec.com>
2018-06-27 22:01:10 +0200

    Fixed some typos

--- origin-cluster-up.sh
+++ origin-cluster-up.sh
@@ -6,7 +6,7 @@
 NODECOUNT=2		  # Set the number of worker nodes, minimum 2
 AZURE_RG=origin		  # Set the name of the resource group for the cluster resources, except the keyvault
 			  # This name will be used for the resource group for the keyvault
-AZURE_LOC="West Europe"	  # Set the location where you want to deploy the cluster
+AZURE_LOC="westeurope"	  # Set the location where you want to deploy the cluster
 AADCLIENT_ID="xxxxx"	  # Set your service principal ID (appId)
 AADCLIENT_SECRET="xxxxx"  # Set your service principal (password)
 
@@ -16,9 +16,9 @@ PASSWORD=$(head -n 50 /dev/urandom | tr -dc 'a-zA-Z0-9-_@#%^_+:?' | fold -w 12 |
 ##
 ## Here be dragons
 ##
-KEYVAULT_RG=$(AZURE_RG)-vault
-KEYVAULT_NAME=$(AZURE_RG)-vault
-KEYVAULT_SECRET=$(AZURE_RG)-key
+KEYVAULT_RG=$AZURE_RG-vault
+KEYVAULT_NAME=$AZURE_RG-vault
+KEYVAULT_SECRET=$AZURE_RG-key
 SSHKEY_NAME=origin-ssh-key
 
 if [[ ! -f origin-ssh-key ]]; then
@@ -53,7 +53,7 @@ sed -e "s#NODECOUNT#$NODECOUNT#;
 echo "##"
 echo "## Launching the cluster deployment"
 echo "##"
-az group create --name $AZURE_RG --location $AZURE_LOG
+az group create --name $AZURE_RG --location $AZURE_LOC
 az group deployment create --resource-group $AZURE_RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json --no-wait 
 
 echo "## Deployment has launched and will take around 30min. to finish"
