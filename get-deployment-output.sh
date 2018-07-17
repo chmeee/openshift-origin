7c1c953606b983d249dc8c09e3eea9c7da17cf47
José Manuel Pavón Álvarez <jmpavon@intellimentsec.com>
2018-07-17 11:52:54 +0100

    Fixed typo

--- get-deployment-output.sh
+++ get-deployment-output.sh
@@ -3,4 +3,4 @@
 ## Load variables from config file
 source origin-cluster.conf
 
-az group deployment show -g $AZURE_RG -n azuredeploy --query 'properties.outputs' | sed 's/ssh -p/ssh -i origin-ssh-key-p/' | grep -v String
+az group deployment show -g $AZURE_RG -n azuredeploy --query 'properties.outputs' | sed 's/ssh -p/ssh -i origin-ssh-key -p/' | grep -v String
